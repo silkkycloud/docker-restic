69ac4d3170bb3985984fef0551636059e588e9a1
Oscar <silkky@silkky.cloud>
2021-12-18 22:25:49 +1100

    Don't create repository

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,16 +7,8 @@ status=$?
 echo "Checking repo status $status"
 
 if [ $status != 0 ]; then
-  echo "restic repository '${RESTIC_REPOSITORY}' does not exist. Running restic init."
-  restic init
-
-  init_status=$?
-  echo "Repo init status $init_status"
-
-  if [ $init_status != 0 ]; then
-    echo "Failed to init the repository: '${RESTIC_REPOSITORY}'"
-    exit 1
-  fi
+  echo "restic repository '${RESTIC_REPOSITORY}' does not exist. You will need to create the repository before using this container."
+  exit 1
 fi
 
 echo "Setting up backup cron job the expression '${BACKUP_CRON}'"
